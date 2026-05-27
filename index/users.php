262a9138173e816a98d9fd37e2682afa4d35b938
DaRk_King <kopytko001234@gmail.com>
2026-05-28 00:20:48 +0300

    Update users.php

--- index/users.php
+++ index/users.php
@@ -1,15 +1,9 @@
 <?php
-// Массив пользователей с дополнительными данными (Логин => Пароль, ФИО, Email)
 $users = array(
     "admin" => array(
         "pwd" => "123456",
         "name" => "Царский Никита Сергеевич",
         "email" => "nikita_001234@list.ru"
-    ),
-    "user1" => array(
-        "pwd" => "qwerty",
-        "name" => "Иванов Иван Иванович",
-        "email" => "ivanov@mail.ru"
     )
 );
 ?>
