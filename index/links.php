fb021226142ba333c72c51f0d81481a750d5874d
DaRk_King <kopytko001234@gmail.com>
2026-05-28 00:22:27 +0300

    Update links.php

--- index/links.php
+++ index/links.php
@@ -1,5 +1,7 @@
-        <h2>Ссылки</h2>
-        <ul>
-          <li><a href="http://yandex.ua" target="_blank">Яндекс</a></li>
-          <li><a href="http://google.com.ua" target="_blank">Google</a></li>
-        </ul>
+<div class="block">
+  <h2>РЎСЃС‹Р»РєРё</h2>
+  <ul>
+    <li><a href="https://google.com" target="_blank">Google</a></li>
+    <li><a href="https://github.com" target="_blank">GitHub</a></li>
+  </ul>
+</div>
